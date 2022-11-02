bc3f036525730ff7aa1b7fef5bc0daa3c7199245
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-11-02 20:48:04 +0600

    add

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import axios from 'axios';
 
 const uris = ['https://bee-volunteers-backend.herokuapp.com', 'http://localhost:3001']
-axios.defaults.baseURL = uris[1];
+axios.defaults.baseURL = uris[0];
 
 
 ReactDOM.render(
